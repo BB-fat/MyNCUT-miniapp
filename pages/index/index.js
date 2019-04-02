8b48ca2031029a4ec5f0a2cd897b22090caa7881
poltawa <qimosaid@163.com>
2019-04-02 20:06:48 +0800

    index fourth

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,50 +12,109 @@ Page({
    * 页面的初始数据
    */
   data: {
+    btn1:{
+      btnWidth:299,
+      btnHeight:192,
+      mgtopbtn:6,
+      fontsize:35,
+      tapFunc:"toClassTable",
+      mgtopimg:10,
+      imgWidth:95,
+      imgHeight:106,
+      imgUrl:"../../imgs/index/course.png",
+      pdtoptxt:40,
+      mglefttxt:22,          
+      btnTxt:"课表"
+    },
+    btn2: {
+      btnWidth: 302,
+      btnHeight: 152,
+      mgleftbtn: 44,
+      fontsize: 30,
+      tapFunc: "toGradePoint",
+      imgWidth: 69,
+      imgHeight: 69,
+      mgtopimg: 15,     
+      imgUrl: "../../imgs/index/average.png",      
+      mglefttxt: 22,
+      pdtoptxt: 10,
+      btnTxt: "绩点"
+    },
+    btn3: {
+      position:"absolute",
+      btnWidth: 299,
+      btnHeight: 105,
+      mgtopbtn: 36,
+      fontsize: 30,
+      tapFunc: "toGrade",
+      imgWidth: 51,
+      imgHeight: 51,
+      mgtopimg: 22,
+      imgUrl: "../../imgs/index/grade.png",
+      mglefttxt: 11,
+      pdtoptxt: 5,
+      btnTxt: "成绩"
+    },
+    btn4: {
+      position: "absolute",
+      btnWidth: 129,
+      btnHeight: 135,
+      mgtopbtn: 7,
+      mgleftbtn:345,
+      fontsize: 25,
+      tapFunc: "toGraduate",
+      imgWidth: 77,
+      imgHeight: 57,
+      imgViewW:120,
+      imgViewH:50,
+      mgtopimgView: 19,
+      imgUrl: "../../imgs/index/graduate.png",
+      txtWidth:120,
+      btnTxt: "毕业要求"
+    },
+    btn5: {
+      position: "absolute",
+      btnWidth: 129,
+      btnHeight: 135,
+      mgtopbtn: 7,
+      mgleftbtn: 518,
+      fontsize: 25,
+      tapFunc: "toClassroom",
+      imgWidth: 64,
+      imgHeight: 56,
+      imgViewW: 120,
+      imgViewH: 50,
+      mgtopimgView: 19,
+      imgUrl: "../../imgs/index/room.png",
+      txtWidth: 120,
+      mgtoptxt:2,
+      btnTxt: "教室"
+    },
     indidots: true,
     idcolor: "#FFFFFF",
     autoplay: true,
-    circular: true,
-    pic_curPage: 0,
+    circular: true,    
     interval: 2600,
     duration: 1000,
-    imgitem: [{
-        link: '/pages/login/login',
-        imgUrl: '../../imgs/index/1.jpg'
-      },
-      {
-        link: '/pages/login/login',
-        imgUrl: '../../imgs/index/2.jpg'
-      },
-      {
-        link: '/pages/login/login',
-        imgUrl: '../../imgs/index/3.jpg'
-      }
-    ],
     notice: '../../imgs/index/notice.png',
-    word_curPage: 0,
-    worditem: [{
-        link: '/pages/login/login',
-        txt: 'Hello World'
-      },
-      {
-        link: '/pages/login/login',
-        txt: '你好 世界'
-      },
-      {
-        link: '/pages/login/login',
-        txt: 'こんにちは、世界'
-      }
-
-    ]
-
-
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    var that=this
+    wx.request({
+
+      url: myURL+'/publicinfo',
+      success(res){        
+        that.setData({
+          indexBanner:res.data.indexBanner,
+          indexNotice:res.data.indexNotice
+        })
+      }
+    })
 
   },
 
@@ -139,10 +198,10 @@ Page({
   },
 
   //轮播图点击事件
-  swipclick: function(e) {
-    var url = e.currentTarget.dataSet.url
+  swipclick: function(e) {    
+    var url = '../webview/webview?mode=normal&url='+this.data.indexBanner
     wx.navigateTo({
-      url: '../login/login'
+      url: url
     })
   },
 
