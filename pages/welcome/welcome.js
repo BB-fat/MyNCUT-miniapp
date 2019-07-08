7edb84d5794a55c1cddfd61ccb0748559be12da9
HongduoXu <46388721+HongduoXu@users.noreply.github.com>
2019-07-08 18:29:02 +0800

    test

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,66 +1,19 @@
-// pages/welcome/welcome.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    btn1: [{
+      img: "cloud://incut-dev-hzz83.696e-incut-dev-hzz83/IMG/welcome/证件.svg",
+      text: "证件"
+    }, {
+      img: "",
+      text: ""
+    }, {
+      img: "",
+      text: ""
+    }],
+    banner: [{
+      image: "",
+      url: ""
+    }],
+    grids: [1, 2, 3, 4]
   }
 })
