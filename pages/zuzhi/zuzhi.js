6a813d3cb58e49a912ce2bf60bea7ccfad6d786e
HongduoXu <46388721+HongduoXu@users.noreply.github.com>
2019-07-10 15:23:07 +0800

    dev

---
+++ pages/zuzhi/zuzhi.js
@@ -0,0 +1,11 @@
+Page({
+  data: {
+    system: [{
+      img: "cloud://incut-dev-hzz83.696e-incut-dev-hzz83/IMG/system/电控.jpg",
+      url:""
+    }, {
+      img: "cloud://incut-dev-hzz83.696e-incut-dev-hzz83/IMG/system/景观.jpg",
+      url:""
+    }]
+  }
+})
