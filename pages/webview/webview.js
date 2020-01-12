ae370675077c72b5b3babd710643a28bbf9ed2b3
鞠欣诚 <bbfat3047@qq.com>
2020-01-12 09:28:05 +0800

    修复成绩按钮无效的问题

--- pages/webview/webview.js
+++ pages/webview/webview.js
@@ -1,28 +1,17 @@
 // pages/webview/webview.js
-import{
-  myURL
-}from '../../setting.js'
 Page({
 
   /**
    * Page initial data
    */
-  data: {
-    oauthUrl: 'https://ucpay.ncut.edu.cn/open/user/oauth/index?redirect='+myURL+'/login/oauth&appid=31b1e992583074382&state=',
-  },
+  data: {},
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function(options) {
-    if (options.mode == "oauth") {
-      this.setData({
-        url: this.data.oauthUrl + options.openid
-      })
-    } else if (options.mode == "normal") {
-      this.setData({
-        url: options.url
-      })
-    }
+  onLoad: function (options) {
+    this.setData({
+      url: options.url
+    })
   },
 })
